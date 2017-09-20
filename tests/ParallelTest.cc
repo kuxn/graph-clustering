f4bf958ba45c87fd3096614d35bfc973c5ba9a1e
Ken Hu <xnchnhu@gmail.com>
2017-09-20 21:20:09 +0100

    Clean up gtests

--- tests/ParallelTest.cc
+++ tests/ParallelTest.cc
@@ -1,5 +1,5 @@
 /**
- * @file test.cc
+ * @file ParallelTest.cc
  * @brief Functions for unit tests
  * @author Ken Hu, xnchnhu@gmail.com
  */
@@ -18,22 +18,33 @@ using namespace std;
 class ParallelTest : public ::testing::Test
 {
 protected:
-    virtual void SetUp() {}
+    virtual void SetUp()
+    {
+        char* path = std::getenv("TEST_FILES");
+        if (path != nullptr) {
+            filePath = path;
+        } else {
+            std::cout << "Please export TEST_FILES to the path of test files"
+                      << std::endl;
+            exit(-1);
+        }
+    }
+    Graph g;
+    std::string filePath;
+    mpi::communicator world;
 };
 
 /**
  * @brief Partitioning is based on the correctness of the calculation of
- * eigenvalues and corresponding eigenvectors of the Laplacian matrix
- * @param FILL-ME-IN
- * @return FILL-ME-IN
+ *        eigenvalues and corresponding eigenvectors of the Laplacian matrix
  */
-TEST(ParallelTest, ReadDotFile) {
-    mpi::communicator world;
-    Graph g;
-    int num = 500, rank = 0;
-    g.readDotFormat("../tests/dotfiles/par_test_500.dot", num);
+TEST_F(ParallelTest, ReadDotFilReadDotFilee)
+{
+    int num = 500;
+    int rank = 0;
+    g.readDotFormat(filePath + "/par_test_500.dot", num);
     if (world.rank() == rank) {
-        //g.printDotFormat();
+        // g.printDotFormat();
         cout << "rank = " << world.rank() << endl;
         cout << "size of graph = " << g.size() << endl;
         cout << "num of edges = " << g.edgesNum() << endl;
@@ -42,7 +53,72 @@ TEST(ParallelTest, ReadDotFile) {
     ASSERT_TRUE(true);
 }
 
-int main(int argc, char** argv) {
+TEST_F(ParallelTest, testPartition)
+{
+    int num = 8;
+    int rank = 0;
+    g.readDotFormat(filePath + "/par_test_8.dot", num);
+    Partition partition(g, 4, true);
+    if (world.rank() == rank) {
+        partition.printLapEigenvalues();
+        partition.printLapEigenMat();
+        // g.printDotFormat();
+    }
+}
+
+/**
+ * @brief Test the read function, can not verify the correctness in unit
+ *        testing, but can test the performance
+ */
+TEST_F(ParallelTest, testReadGraph)
+{
+    int num = 500;
+    int rank = 0;
+    g.readDotFormat(filePath + "/par_test_500.dot", num);
+    if (world.rank() == rank) {
+        // g.printDotFormat();
+        cout << "rank = " << world.rank() << endl;
+        cout << "size of graph = " << g.size() << endl;
+        cout << "num of edges = " << g.edgesNum() << endl;
+        // g.printLaplacianMat();
+    }
+}
+
+/**
+ * @brief Test reading the vertices with same colours and corresponding edges
+ */
+TEST_F(ParallelTest, testReadGraphByColour)
+{
+    int num = 20;
+    int rank = 3;
+    g.readDotFormatByColour(filePath + "/par_test_20_4s.dot", num);
+    if (world.rank() == rank) {
+        cout << "rank = " << world.rank() << endl;
+        cout << "size of graph = " << g.size() << endl;
+        cout << "num of edges = " << g.edgesNum() << endl;
+        g.printDotFormat();
+    }
+}
+
+/**
+ * @brief Cluster assignment: test the partition with partitioned graph
+ */
+TEST_F(ParallelTest, testPartitionWithClusters)
+{
+    int num = 1024;
+    int subgraphs = 4;
+    bool gram_schmidt = true;
+    g.readDotFormatByColour(filePath + "/par_test_1024_4s.dot", num);
+    if (world.rank() == 0) {
+        // g.printDotFormat();
+        cout << "rank = " << world.rank() << ", size of graph = " << g.size()
+             << ", num of edges = " << g.edgesNum() << endl;
+    }
+    Partition partition(g, subgraphs, gram_schmidt);
+}
+
+int main(int argc, char** argv)
+{
     int result = 0;
     ::testing::InitGoogleTest(&argc, argv);
     mpi::environment env;
@@ -51,103 +127,12 @@ int main(int argc, char** argv) {
     ::testing::TestEventListeners& listeners =
         ::testing::UnitTest::GetInstance()->listeners();
 
-    // Only leave the listener for rank 0, such that the messages dorn't mingle
+    // Only leave the listener for rank 0, such that the messages don't mingle
     if (world.rank() != 0) {
         delete listeners.Release(listeners.default_result_printer());
     }
 
+    result = RUN_ALL_TESTS();
+    env.~environment();
     return result;
 }
-
-//bool Tests::testPartition()
-//{
-//    mpi::environment env;
-//    mpi::communicator world;
-//    Graph g;
-//    ifstream In("./test/par_test_8.dot");
-//    if (!In.is_open()) {
-//        std::cerr << "ERROR: Can't open the file" << endl;
-//        exit(-1);
-//    }
-//    Partition partition(g, 4, true);
-//    if (world.rank() == 0) {
-//        partition.printLapEigenvalues();
-//        partition.printLapEigenMat();
-//        g.printDotFormat();
-//    }
-//    env.~environment();
-//    return true;
-//}
-//
-///**
-// * @brief Test the read function, can not verify the correctness in unit
-// * testing, but can test the performance
-// * @param FILL-ME-IN
-// * @return FILL-ME-IN
-// */
-//
-//bool Tests::testReadGraph()
-//{
-//    mpi::environment env;
-//    mpi::communicator world;
-//    Graph g;
-//    int num = 500, rank = 0;
-//    g.readDotFormat("./test/par_test_500.dot", num);
-//    if (world.rank() == rank) {
-//        g.printDotFormat();
-//        cout << "rank = " << world.rank() << endl;
-//        cout << "size of graph = " << g.size() << endl;
-//        cout << "num of edges = " << g.edgesNum() << endl;
-//        // g.printLaplacianMat();
-//    }
-//    env.~environment();
-//    return true;
-//}
-//
-///**
-// * @brief Test reading the vertices with same colours and corresponding edges
-// * @param FILL-ME-IN
-// * @return FILL-ME-IN
-// */
-//
-//bool Tests::testReadByColour()
-//{
-//    mpi::environment env;
-//    mpi::communicator world;
-//    Graph g;
-//    int num = 20, rank = 3;
-//    g.readDotFormatByColour("./test/par_test_20_4s.dot", num);
-//    if (world.rank() == rank) {
-//        cout << "rank = " << world.rank() << endl;
-//        cout << "size of graph = " << g.size() << endl;
-//        cout << "num of edges = " << g.edgesNum() << endl;
-//        g.printDotFormat();
-//    }
-//    env.~environment();
-//    return true;
-//}
-//
-///**
-// * @brief Cluster assignment: test the partition with partitioned graph
-// * @param FILL-ME-IN
-// * @return FILL-ME-IN
-// */
-//
-//bool Tests::testPartitionWithClusters()
-//{
-//    mpi::environment env;
-//    mpi::communicator world;
-//    Graph g;
-//    int num = 1024, subgraphs = 4;
-//    bool gram_schmidt = true;
-//    g.readDotFormatByColour("./test/par_test_1024.dot", num);
-//    if (world.rank() == 0) {
-//        // g.printDotFormat();
-//        cout << "rank = " << world.rank() << ", size of graph = " << g.size()
-//             << ", num of edges = " << g.edgesNum() << endl;
-//    }
-//    Partition partition(g, subgraphs, gram_schmidt);
-//    env.~environment();
-//    return true;
-//}
-
